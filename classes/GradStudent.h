708058c464a178c1f7340e910e80f3b56f55a4b6
Graham Albachten <albachteng@gmail.com>
2023-05-07 10:44:44 -0500

    inherit as private and override with using

--- classes/GradStudent.h
+++ classes/GradStudent.h
@@ -2,11 +2,15 @@
 #include "Student.h"
 #include <string>
 
-class GraduateStudent : public Student {
+class GraduateStudent : private Student {
 private:
   bool scholarship;
 
 public:
+  using Student::get_gpa;
+  using Student::get_name;
+  using Student::set_gpa;
+  using Student::set_name;
   GraduateStudent() = default;
   bool has_scholarship() const;
   void set_scholarship(bool b);
